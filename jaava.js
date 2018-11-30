2759f0232b8a264d831436cb657c8d5d0c26c453
LauriLomp <laurilomp@hotmail.com>
2018-11-30 18:53:30 +0200

    Tehtud

---
+++ jaava.js
@@ -0,0 +1,77 @@
+var image_tracker = "f";
+function change(){
+  var image = document.getElementById("map");
+  if(image_tracker=="f"){
+    image.src ="miragebsite.png";
+    image_tracker = "t";
+  }else{
+    image.src ="https://static.hltv.org/images/galleries/1693-medium/1370663191.8834.jpeg";
+    image_tracker = "f";
+  }
+}
+var image_tracker2 = "f";
+function change2(){
+  var image = document.getElementById("map2");
+  if(image_tracker2=="f"){
+    image.src ="https://steamcdn-a.akamaihd.net/apps/csgo/images/dust2/small_screens/shot10a.jpg";
+    image_tracker2 = "t";
+  }else{
+    image.src ="https://www.kitguru.net/wp-content/uploads/2017/10/Dust-2-A-Old.jpg";
+    image_tracker2 = "f";
+  }
+}
+var image_tracker3 = "f";
+function change3(){
+  var image = document.getElementById("map3");
+  if(image_tracker3=="f"){
+    image.src ="http://tsarvar.com/maps/csgo/de_cache/1.jpg";
+    image_tracker3 = "t";
+  }else{
+    image.src ="https://vignette.wikia.nocookie.net/cswikia/images/4/4e/Csgo-de-cache.png";
+    image_tracker3 = "f";
+  }
+}
+var image_tracker4 = "f";
+function change4(){
+  var image = document.getElementById("map4");
+  if(image_tracker4=="f"){
+    image.src ="https://image.redbull.com/rbcom/010/2016-02-26/1331779081975_3/0100/0/1/the-view-from-heaven-with-half-the-rafters-barred-off-in-the-revamped-nike-map-in-cs-go.jpg";
+    image_tracker4 = "t";
+  }else{
+    image.src ="https://vignette.wikia.nocookie.net/cswikia/images/5/51/De_nuke_thumbnail.jpg";
+    image_tracker4 = "f";
+  }
+}
+var image_tracker5 = "f";
+function change5(){
+  var image = document.getElementById("map5");
+  if(image_tracker5=="f"){
+    image.src ="https://cs.ingame.de/files/2015/04/de_overpass-a-spot-opened-up.jpg";
+    image_tracker5 = "t";
+  }else{
+    image.src ="https://vignette.wikia.nocookie.net/cswikia/images/6/6e/Csgo-de-overpass.png";
+    image_tracker5 = "f";
+  }
+}
+var image_tracker6 = "f";
+function change6(){
+  var image = document.getElementById("map6");
+  if(image_tracker6=="f"){
+    image.src ="https://i2.wp.com/gamingph.com/wp-content/uploads/2016/10/14681991_1119985091371012_3411199847805761612_o.jpg";
+    image_tracker6 = "t";
+  }else{
+    image.src ="https://vignette.wikia.nocookie.net/cswikia/images/f/f0/Inferno.jpg";
+    image_tracker6 = "f";
+  }
+}
+var image_tracker7= "f";
+function change7(){
+  var image = document.getElementById("map7");
+  if(image_tracker7=="f"){
+    image.src ="https://steamcdn-a.akamaihd.net/apps/csgo/images/reintroducing_train/BSiteOverview-new.jpg";
+    image_tracker7 = "t";
+  }else{
+    image.src ="https://vignette.wikia.nocookie.net/cswikia/images/4/4a/De_train_thumbnail.png";
+    image_tracker7 = "f";
+  }
+}
