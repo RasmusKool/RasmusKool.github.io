fc24dcc01064d35cdd69dce16cff40177c1db75d
RasmusKool <44945203+RasmusKool@users.noreply.github.com>
2018-12-24 14:18:07 +0200

    Delete parallax.js

--- parallax.js
+++
@@ -1,5 +0,0 @@
-var currentDate = new Date(),
-      day = currentDate.getDate(),
-      month = currentDate.getMonth() + 1,
-      year = currentDate.getFullYear(),
-      date = day + "/" + month + "/" + year;
